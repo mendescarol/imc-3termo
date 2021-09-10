ff4ed81a3e318e88602e76e01b08ce3ad9bfe198
Carol Mendes <mendescarol920@gmail.com>
2021-09-10 11:25:29 -0300

    atualizado

--- app/src/main/java/br/senai/sp/jandira/imcapp20_a/ui/DashBoardActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/imcapp20_a/ui/DashBoardActivity.kt
@@ -3,6 +3,9 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.cardview.widget.CardView
 import br.senai.sp.jandira.imcapp20_a.R
 import br.senai.sp.jandira.imcapp20_a.utils.converterBase64ParaBitmap
 import com.bumptech.glide.Glide
@@ -11,6 +14,26 @@ class DashBoardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash_board)
+
+        val alertDialog = AlertDialog.Builder(this@DashBoardActivity).create()
+        alertDialog.setTitle("Cadastro não finalizado")
+        alertDialog.setMessage("Você não terminou o seu cadastro, deseja concluir ? ")
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Sim", {dialogInterface, i ->
+            Toast.makeText(applicationContext, "Ok  clicado", Toast.LENGTH_SHORT).show()
+
+
+        })
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancelar", {dialogInterface, i ->
+            Toast.makeText(applicationContext, "Cancelar clicado", Toast.LENGTH_SHORT).show()
+        })
+
+
+
+        alertDialog.show()
+
+
+
+
         preencherDashBoard()
         tv_logout.setOnClickListener {
             val dados = getSharedPreferences("dados_usuario", Context.MODE_PRIVATE)
@@ -34,4 +57,8 @@ class DashBoardActivity : AppCompatActivity() {
 
 
     }
+
+
+
+
 }
